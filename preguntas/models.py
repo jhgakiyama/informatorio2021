1041544c33b2bb004a663fde1d710ff2ed2836ca
jhgakiyama <lp_japo@hotmail.com>
2021-08-21 11:32:27 -0300

    update model Pregunta

--- preguntas/models.py
+++ preguntas/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 
 class Pregunta(models.Model):
-    pass
+    CATEGORIA_CHOICES = [
+        (1, 'Cultura y arte'),
+        (2, 'Historia'),
+        (3, 'Deporte'),
+        (4, 'Geografía'),
+        (5, 'Economía'),
+        (6, 'Ciencia y Educación'),
+        (7, 'Entretenimiento')
+    ]
+
+    categoria = models.PositiveSmallIntegerField(
+        'categoria',
+        choices=CATEGORIA_CHOICES,
+        default=1
+    )
 
 
 class Respuesta(models.Model):
