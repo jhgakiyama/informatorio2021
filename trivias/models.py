50371d8b93892249e3636bcfccb835fe3370cab7
jhgakiyama <lp_japo@hotmail.com>
2021-08-21 11:31:43 -0300

    agrego model Trivia

--- trivias/models.py
+++ trivias/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Trivia(models.Model):
+    NIVEL_CHOICES = [
+        (1, 'Facil'),
+        (2, 'Normal'),
+        (3, 'Dificil')
+    ]
+
+    nivel = models.PositiveSmallIntegerField(
+        'dificultad',
+        help_text="Nivel de dificultad",
+        choices=NIVEL_CHOICES,
+        default=1
+    )
+
+    user = models.ForeignKey(
+        'usuario',
+        User,
+        on_delete=models.CASCADE
+    )
