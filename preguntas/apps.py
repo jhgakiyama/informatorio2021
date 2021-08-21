3179f53685fe39d527c7bcd8a0994aba39b07933
jhgakiyama <lp_japo@hotmail.com>
2021-08-20 21:02:44 -0300

    first commit

---
+++ preguntas/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PreguntasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'preguntas'
+    verbose_name = 'Administrar Preguntas y Respuestas'
