57caf7df3ea3649fc68e984dbfa8482a8e2852db
jhgakiyama <lp_japo@hotmail.com>
2021-08-21 10:31:46 -0300

    agrego model pregunta y respuesta

--- preguntas/models.py
+++ preguntas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pregunta(models.Model):
+    pass
+
+
+class Respuesta(models.Model):
+    texto = models.CharField(max_length=120)
+    correcta = models.BooleanField(default=False)
+    pregunta = models.ForeignKey(
+        Pregunta,
+        on_delete=models.CASCADE,
+        related_name='respuesta'
+    )
+
+    def __str__(self):
+        return f"Pregunta: {self.pregunta.texto}, Rta: {self.texto}, Correcta: {self.correcta}"
