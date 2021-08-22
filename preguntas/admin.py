323f1d018407d0b3652aa86976368dbf2f742dc8
jhgakiyama <lp_japo@hotmail.com>
2021-08-22 19:58:47 -0300

    update admin de Preguntas

--- preguntas/admin.py
+++ preguntas/admin.py
@@ -6,9 +6,18 @@ class RespuestaInLine(admin.TabularInline):
     model = Respuesta
 
 
+@admin.register(Pregunta)
 class PreguntaAdmin(admin.ModelAdmin):
     inlines = [RespuestaInLine]
+    list_display = ["texto", "categoria"] # columnas que muestra la grilla
+    search_fields = ["texto"]  # Buscado encima
+    list_filter = ["categoria"]  # Filtro a la derecha
+    ordering = ["id"]
+    list_per_page = 8
 
 
-admin.site.register(Pregunta, PreguntaAdmin)
-admin.site.register(Respuesta)
+@admin.register(Respuesta)
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ["texto", "pregunta", "correcta"]
+    ordering = ["pregunta", "-correcta"]
+    list_filter = ["correcta"]  # Filtro a la derecha
