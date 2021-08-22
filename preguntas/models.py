c65c41ea8bb5d28bb1b01c30a440aec3069a3a31
jhgakiyama <lp_japo@hotmail.com>
2021-08-22 20:00:49 -0300

    update model Pregunta

--- preguntas/models.py
+++ preguntas/models.py
@@ -1,25 +1,32 @@
 from django.db import models
+import random
 
 
 class Pregunta(models.Model):
     CATEGORIA_CHOICES = [
-        (1, 'Cultura y arte'),
-        (2, 'Historia'),
+        (1, 'Ciencia y Educación'),
+        (2, 'Cultura y arte'),
         (3, 'Deporte'),
-        (4, 'Geografía'),
-        (5, 'Economía'),
-        (6, 'Ciencia y Educación'),
-        (7, 'Entretenimiento')
+        (4, 'Economía'),
+        (5, 'Entretenimiento'),
+        (6, 'Historia'),
+        (7, 'Geografía'),
     ]
 
+    texto = models.CharField(max_length=120)
     categoria = models.PositiveSmallIntegerField(
         'categoria',
         choices=CATEGORIA_CHOICES,
-        default=1
+        default=6
     )
 
     def __str__(self):
-        return str(self.texto)
+        return f"{self.texto}"
+
+    def get_respuestas(self):
+        respuestas = list(Respuesta.objects.filter(pregunta=self.id))
+        random.shuffle(respuestas)
+        return respuestas
 
 
 class Respuesta(models.Model):
