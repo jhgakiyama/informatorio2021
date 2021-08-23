1ecf7da7c34fc314889cc85960dfd5aa8a6eab7e
jhgakiyama <lp_japo@hotmail.com>
2021-08-22 22:31:50 -0300

    agrego metodo para obtener nro de respuestas

--- preguntas/models.py
+++ preguntas/models.py
@@ -23,20 +23,32 @@ class Pregunta(models.Model):
     def __str__(self):
         return f"{self.texto}"
 
-    def get_respuestas(self):
+    def get_respuestas_random(self):
         respuestas = list(Respuesta.objects.filter(pregunta=self.id))
         random.shuffle(respuestas)
         return respuestas
 
+    def cant_rtas(self):
+        return Respuesta.objects.filter(pregunta=self.id).count()
+    cant_rtas.short_description = "Respuestas"
+
+    def cant_rtas_ok(self):
+        return Respuesta.objects.filter(pregunta=self.id, correcta=True).count()
+    cant_rtas_ok.short_description = "Rtas Correctas"
+
 
 class Respuesta(models.Model):
     texto = models.CharField(max_length=120)
-    correcta = models.BooleanField(default=False)
     pregunta = models.ForeignKey(
         Pregunta,
         on_delete=models.CASCADE,
         related_name='respuesta'
     )
+    correcta = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Pregunta: {self.pregunta.texto}, Rta: {self.texto}, Correcta: {self.correcta}"
+
+    def short(self):
+        return f"{self.pregunta.texto[:60]} ..."
+    short.short_description = "Pregunta"
