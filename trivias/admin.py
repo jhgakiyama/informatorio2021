d35938336203e42cebac7e9d200d3af975d45442
jhgakiyama <lp_japo@hotmail.com>
2021-08-23 08:12:35 -0300

    update admin de Trivia

--- trivias/admin.py
+++ trivias/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trivia
 
-# Register your models here.
+
+@admin.register(Trivia)
+class TriviaAdmin(admin.ModelAdmin):
+    list_display = ["user", "nivel", "fecha"]
+    list_filter = ["nivel"]
