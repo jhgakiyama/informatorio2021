0cfa1c9c71c308095a5d02fe1d18a4692da820eb
jhgakiyama <lp_japo@hotmail.com>
2021-08-26 19:46:39 -0300

    queriendo subir a heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.contrib.messages import constants as message_constants
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +91,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+
     }
 }
 
