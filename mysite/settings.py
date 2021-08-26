0fc1d43946ccf016311b6966958d421cf973c352
jhgakiyama <lp_japo@hotmail.com>
2021-08-26 19:44:15 -0300

    queriendo subir a heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -166,3 +166,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-import whitenoise
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
 
 application = get_wsgi_application()
-
-
-application = WhiteNoise(application)
