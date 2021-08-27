6f3391e54ad54930899748905e720e97fd9fccf0
jhgakiyama <lp_japo@hotmail.com>
2021-08-27 20:54:09 -0300

    quito respuesta del admin

--- preguntas/admin.py
+++ preguntas/admin.py
@@ -9,19 +9,20 @@ class RespuestaInLine(admin.TabularInline):
 @admin.register(Pregunta)
 class PreguntaAdmin(admin.ModelAdmin):
     inlines = [RespuestaInLine]
-    list_display = ["texto", "categoria", "cant_rtas", "cant_rtas_ok"]  # columnas que muestra la grilla
+    list_display = ["id", "texto", "categoria", "cant_rtas", "cant_rtas_ok"]  # columnas que muestra la grilla
     search_fields = ["texto"]  # Buscado encima
     list_filter = ["categoria"]  # Filtro a la derecha
     ordering = ["id"]
     list_per_page = 8
 
 
-@admin.register(Respuesta)
-class RespuestaAdmin(admin.ModelAdmin):
-    list_display = ["texto", "short", "correcta"]
-    ordering = ["pregunta", "-correcta"]
-    list_filter = ["correcta"]  # Filtro a la derecha
-    list_per_page = 9
+# @admin.register(Respuesta)
+# class RespuestaAdmin(admin.ModelAdmin):
+#     list_display = ["texto", "short", "correcta"]
+#     search_fields = ["pregunta.text"]  # Buscado encima
+#     ordering = ["pregunta", "-correcta"]
+#     list_filter = ["correcta"]  # Filtro a la derecha
+#     list_per_page = 9
 
 
 admin.site.site_header = "Trivia Chaco 2021 | Admin Ver 1.0.0"
