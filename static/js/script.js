c4c6538c9c47b2a5bb1b29a68107efb123d60cbc
Marigiko <86132907+Marigiko@users.noreply.github.com>
2021-09-03 15:28:55 -0300

    script disabled finalizado

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,14 @@
 function correcto(a, c) {
     document.getElementById(a).style.backgroundColor = "#d1e7dd";
-    // document.getElementById(c).disabled = true;
     let nodes = document.getElementById(c).getElementsByTagName('*');
-    for(var i = 0; i < nodes.length; i++){
+    for(let i = 0; i < nodes.length; i++){
         nodes[i].disabled = true;
     }
 }
 function incorrecto(b, c) {
     document.getElementById(b).style.backgroundColor = "#f8d7da";
-    // document.getElementById(c).disabled = true;
-    var nodes = document.getElementById(c).getElementsByTagName('*');
-    for(var i = 0; i < nodes.length; i++){
+    let nodes = document.getElementById(c).getElementsByTagName('*');
+    for(let i = 0; i < nodes.length; i++){
         nodes[i].disabled = true;
     }
 }
