6fe5d4cae4ed750a8592bd2c3929b283d4094dc8
Marigiko <86132907+Marigiko@users.noreply.github.com>
2021-09-02 22:46:35 -0300

    script en proceso

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,16 @@
-function correcto(a) {
+function correcto(a, c) {
     document.getElementById(a).style.backgroundColor = "#d1e7dd";
+    // document.getElementById(c).disabled = true;
+    let nodes = document.getElementById(c).getElementsByTagName('*');
+    for(var i = 0; i < nodes.length; i++){
+        nodes[i].disabled = true;
+    }
 }
-function incorrecto(b) {
+function incorrecto(b, c) {
     document.getElementById(b).style.backgroundColor = "#f8d7da";
+    // document.getElementById(c).disabled = true;
+    var nodes = document.getElementById(c).getElementsByTagName('*');
+    for(var i = 0; i < nodes.length; i++){
+        nodes[i].disabled = true;
+    }
 }
