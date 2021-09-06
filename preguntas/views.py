729baa2452ddce594233d22f4b018d6b7e15a51d
jhgakiyama <lp_japo@hotmail.com>
2021-09-06 15:53:21 -0300

    vistas para "jugar" preguntas y ver resultado

--- preguntas/views.py
+++ preguntas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Pregunta, Respuesta
+from trivias.models import Juego
+from django.contrib.auth.decorators import login_required
 
 
 class PreguntaListView(ListView):
@@ -12,35 +14,83 @@ class PreguntaDetailView(DetailView):
 
 
 def preguntadetailrandom(request):
-    pregunta = Pregunta.objects.order_by("?").first()
+    # pregunta = Pregunta.objects.order_by("?").first()
+    pregunta = Pregunta.objects.order_by("?")[:10]
     template = "preguntas/pregunta_detail.html"
     return render(request, template, {"object": pregunta})
 
 
-def getPreguntasFacil(request):
+@login_required
+def getpreguntasfacil(request):
     context = {}
     preguntas = Pregunta.objects.filter(nivel=1).order_by("?")[:10]
-    template = "preguntas/pregunta_list.html"
+    # template = "preguntas/pregunta_list.html"
+    template = "preguntas/pregunta_listado.html"
     context["object_list"] = preguntas
-    context["titulo"] = "(Facil)"
+    context["nivel"] = "(Facil)"
     return render(request, template, context)
 
 
-def getPreguntasNormal(request):
+@login_required
+def getreguntasnormal(request):
     context = {}
     preguntas = Pregunta.objects.filter(nivel=2).order_by("?")[:10]
-    template = "preguntas/pregunta_list.html"
+    # preguntas = Pregunta.objects.order_by("?")[:10]
+    # preguntas = Pregunta.objects.filter(nivel=2)[:5]
+    template = "preguntas/pregunta_listado.html"
     context["object_list"] = preguntas
-    context["titulo"] = "(Normal)"
-    return render(request, template, context)
+    context["nivel"] = "(Normal)"
+    return render(request, template_name=template, context=context)
 
 
-def getPreguntasDificil(request):
+@login_required
+def getpreguntasdificil(request):
     context = {}
     preguntas = Pregunta.objects.filter(nivel=3).order_by("?")[:10]
-    template = "preguntas/pregunta_list.html"
+    # template = "preguntas/pregunta_list.html"
+    template = "preguntas/pregunta_listado.html"
     context["object_list"] = preguntas
-    context["titulo"] = "(Dificil)"
+    context["nivel"] = "(Dificil)"
     return render(request, template, context)
 
+
+@login_required
+def resultadopregunta(request):
+    context = {}
+    template = "resultado.html"
+
+    print("Imprimo claves del POST")
+
+    preg_id = []
+    dic_p_r = {}
+    cont_correcta = 0
+    for key, value in request.POST.items():
+        # key es la pregunta_id
+        # value es la respuesta que se eligio
+        if key != 'csrfmiddlewaretoken':
+            # print(f'Pregunta_id: {key}')
+            # print(f'Rta: {value}')
+            rta_selec = Respuesta.objects.get(pk=value)
+            llave = int(key)
+            dic_p_r[llave] = rta_selec
+            preg_id.append(key)
+            pregunta = Pregunta.objects.get(pk=key)
+            if pregunta.respuesta_es_correcta(int(value)):
+                cont_correcta += 1
+
+    # preg_id, es una coleccion de las preguntas_id
+    preguntas_originales = Pregunta.objects.filter(pk__in=preg_id)
+    dificultad = preguntas_originales[0].nivel
+    context['preguntas_originales'] = preguntas_originales
+    context['respuestas_sel'] = dic_p_r
+    context['correctas'] = cont_correcta
+
+    # creo el nuevo juego
+    juego = Juego(
+        user=request.user,
+        nivel=dificultad,
+        puntaje=cont_correcta
+    )
+    juego.save()
+    return render(request, template, context)
 
