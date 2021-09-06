547981690174691129536867bcfc2c84e0a38329
jhgakiyama <lp_japo@hotmail.com>
2021-09-06 15:43:19 -0300

    quito modelo Trivia

--- trivias/admin.py
+++ trivias/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Trivia, PuntajeUsuario
+from .models import Juego
 
-@admin.register(Trivia)
-class TriviaAdmin(admin.ModelAdmin):
-    list_display = ["user", "nivel", "fecha"]
-    list_filter = ["nivel"]
 
-admin.site.register(PuntajeUsuario)
+@admin.register(Juego)
+class JuegoAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "nivel", "fecha", "puntaje"]  # columnas de la grilla
+    list_filter = ["user", "nivel"]
+    list_editable = ["nivel", "puntaje"]
+    list_per_page = 10
+
+# @admin.register(Trivia)
+# class TriviaAdmin(admin.ModelAdmin):
+#     filter_horizontal = ["pregunta"]  # muestro 2 columnas para seleccionar las preguntas
+#     list_display = ["id", "nivel", "fecha"]  # columnas de la grilla
+#     list_filter = ["nivel"]  # filtro a la derecha
+#
+#
