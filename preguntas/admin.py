e8a58e6e31c92b43aaa96d91865869a8890d7240
jhgakiyama <lp_japo@hotmail.com>
2021-09-06 15:42:43 -0300

    agrego campos editables a la Pregunta

--- preguntas/admin.py
+++ preguntas/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from .models import Pregunta, Respuesta
 
 
+# class CantRtasFilter(admin.SimpleListFilter):
+#     """
+#         No me sirve porque los query los tengo que
+#         hacer sobre atributos fisico.
+#     """
+#     title = "# Rtas Verdadera"
+#     parameter_name = "cant_rtas"
+#
+#     def lookups(self, request, model_admin):
+#         return (
+#             # (el valor que devuelve la queryset, lo que va a decir la dcha),
+#             ('1', '1'),
+#             ('2', '2 o mas'),
+#         )
+#
+#     def queryset(self, request, queryset):
+#         if not self.value():
+#             return queryset
+#         if self.value() == '1':
+#             return queryset.filter(cant_rtas=1)
+#         else:
+#             return queryset.exclude(cant_rtas=1)
+#
+
 class RespuestaInLine(admin.TabularInline):
     model = Respuesta
 
@@ -9,7 +33,8 @@ class RespuestaInLine(admin.TabularInline):
 @admin.register(Pregunta)
 class PreguntaAdmin(admin.ModelAdmin):
     inlines = [RespuestaInLine]
-    list_display = ["id", "texto", "nivel", "categoria", "cant_rtas", "cant_rtas_ok"]  # columnas que muestra la grilla
+    list_display = ["id", "texto", "nivel", "categoria", "cant_rtas", "get_respuestas_ok"]  #  muestra la grilla
+    list_editable = ["nivel", "categoria"]  # editor en grilla
     search_fields = ["texto"]  # Buscado encima
     list_filter = ["nivel", "categoria"]  # Filtro a la derecha
     ordering = ["id"]
