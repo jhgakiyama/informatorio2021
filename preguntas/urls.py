50424d9eb02f3de84c3f77079100e33660f4f2a2
jhgakiyama <lp_japo@hotmail.com>
2021-09-06 19:54:51 -0300

    descomento url

--- preguntas/urls.py
+++ preguntas/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('', PreguntaListView.as_view(), name='pregunta-list'),
     path('<int:pk>/', PreguntaDetailView.as_view(), name='pregunta-detail'),
     # path('random/', preguntadetailrandom, name='pregunta-detail-random'),
-    # path('facil/', getpreguntasfacil, name='preguntas-facil'),
-    # path('normal/', getreguntasnormal, name='preguntas-normal'),
-    # path('dificil/', getpreguntasdificil, name='preguntas-dificil'),
+    path('facil/', getpreguntasfacil, name='preguntas-facil'),
+    path('normal/', getreguntasnormal, name='preguntas-normal'),
+    path('dificil/', getpreguntasdificil, name='preguntas-dificil'),
     path('resultado/', resultadopregunta, name='resultado')
 ]
