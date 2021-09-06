bfc02a7fbb687d3da455df59a206a90f099fd03d
jhgakiyama <lp_japo@hotmail.com>
2021-09-06 16:09:33 -0300

    template para elegir el nivel del juego

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import TemplateView, CreateView
+from django.db.models import QuerySet
+from django.views.generic import TemplateView, CreateView, ListView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from .forms import UserCreationForm
+from django.shortcuts import render
+from trivias.models import Juego
 
 
 class HomePageView(TemplateView):
@@ -13,3 +16,42 @@ class SignUpView(SuccessMessageMixin, CreateView):
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
     success_message = '<strong>Cuenta creada!</strong> Ya puedes iniciar sesión'
+
+
+class JugarTemplateView(TemplateView):
+    template_name = 'jugar.html'
+
+
+def add(request):
+    mi_contexto = {}
+    # print("Imprimo claves del POST")
+    # for key, value in request.POST.items():
+    #     print(f'Key: {key}')
+    #     print(f'Value: {value}')
+    #
+    # print(list(request.POST.items()))
+    # print(dict(request.POST.items()))
+    #
+    # print("Imprimo claves del GET")
+    # for key, value in request.GET.items():
+    #     print(f'Key: {key}')
+    #     print(f'Value: {value}')
+    #
+    # print(list(request.GET.items()))
+    # print(dict(request.GET.items()))
+
+    if request.method == 'GET':
+        valor1 = request.GET['num1']
+        valor2 = request.GET['num2']
+        print("Estoy en el GET")
+    else:
+        valor1 = request.POST['num1']
+        valor2 = request.POST['num2']
+        print("Estoy en el POST")
+
+    template = "resultado.html"
+    # suma = int(valor1) + int(valor2)
+
+    mi_contexto['num1'] = int(valor1)
+    mi_contexto['num2'] = int(valor2)
+    return render(request, template_name=template, context=mi_contexto)
